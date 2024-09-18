8ea3f89d4e50b5511cd2cc3a0d2d0715778ee9f1
Insidioused <66269378+Insidioused@users.noreply.github.com>
2024-09-18 09:49:39 +0000

    Added Google Maps

---
+++ assets/js/maps.js
@@ -0,0 +1,9 @@
+function initMap(){
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 10,
+        center: {
+            lat: 51.5095,
+            lng: -0.1245
+        }
+    });
+}
